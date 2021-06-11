28dae2074dd52cb2fa123ee244bb86ee86d0bce8
gupta1996meenakshi <80736409+gupta1996meenakshi@users.noreply.github.com>
2021-06-11 12:47:56 +0530

    new file added

---
+++ CalcA.java
@@ -0,0 +1,8 @@
+public class CalcA implements Calculator{
+    public void addA(int a,int b){
+        System.out.println("Sum By A:"+(a+b));
+    }
+    public void add(int a,int b){
+        System.out.println("Sum By A:"+(a+b));
+    }
+}
